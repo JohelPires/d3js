e9a57e5d4d7b3fc1bfa10e6e9494860e70c7c814
JohelPires <johelpires@gmail.com>
2022-07-11 13:29:59 -0400

    selections and changing attributes

---
+++ script.js
@@ -0,0 +1,7 @@
+d3.select('h1').style('color', 'orangered').text('Data Driven Documents')
+
+d3.select('body').append('p').text('this is a paragraph')
+d3.select('body').append('p').text('this is a second paragraph')
+d3.select('body').append('p').text('this is a third paragraph')
+
+d3.selectAll('p').style('color', 'dodgerblue')
