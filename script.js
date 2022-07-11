e9e291d1920751cbf67408fa4a1d05c81f2430ec
JohelPires <johelpires@gmail.com>
2022-07-11 13:36:24 -0400

    creating new elements

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ d3.select('body').append('p').text('this is a second paragraph')
 d3.select('body').append('p').text('this is a third paragraph')
 
 d3.selectAll('p').style('color', 'dodgerblue')
+
+const dataset = [1, 2, 3, 4, 5]
+
+d3.select('body')
+  .selectAll('div')
+  .data(dataset)
+  .enter()
+  .append('div')
+  .text((d) => `${d}º item added here`)
